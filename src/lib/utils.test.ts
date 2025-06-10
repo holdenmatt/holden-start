2db3d8cc9a43568487da7dae83731e94df9d97eb
Matt Holden <holden.matt@gmail.com>
2025-06-09 21:16:10 -0600

    add example tests

---
+++ src/lib/utils.test.ts
@@ -0,0 +1,14 @@
+import { describe, expect, it } from "vitest";
+
+import { cn } from "./utils";
+
+// Example tests file
+describe("cn", () => {
+  it("should conditionally merge class names", () => {
+    expect(cn("foo", false && "bar", "baz")).toBe("foo baz");
+  });
+
+  it("should handle tailwind conflicts", () => {
+    expect(cn("px-2", "px-4")).toBe("px-4");
+  });
+});
